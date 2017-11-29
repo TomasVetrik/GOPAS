b8f9d3fc37ca409664cde5c7c6b215e6f32abd69
vetrik3 <vetrikt@gmail.com>
2017-11-29 15:26:30 +0100

    Prerobenie Configuracie pre System Locale na slovak

--- Custom_Blava.ps1
+++ Custom_Blava.ps1
@@ -1,2 +1,2 @@
 . D:\Functions.ps1
-Run -Path "C:\Windows\System32\cmd.exe" '/C control "intl.cpl,,/f:D:\temp\Config_SK.xml"'
+Run -Path "C:\Windows\System32\control.exe" 'intl.cpl,,/f:"D:\temp\Config_SK.xml"'
