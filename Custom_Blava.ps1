86d5394c0922f3434998deaec0171a58dde04cfa
vetrik3 <vetrikt@gmail.com>
2019-02-11 11:28:32 +0100

    Update Custom_Blava.ps1

--- Custom_Blava.ps1
+++ Custom_Blava.ps1
@@ -1,5 +1,14 @@
 . D:\Functions.ps1
-Run -Path "C:\Windows\System32\control.exe" 'intl.cpl,,/f:"D:\temp\Config_SK.xml"'
+
+switch -wildcard ($env:USERNAME) 
+{ 
+    "StudentEN" {$Arguments = 'intl.cpl,,/f:"D:\temp\Config_EN.xml"'}
+ 	"StudentSK" {$Arguments = 'intl.cpl,,/f:"D:\temp\Config_SK.xml"'}
+    "StudentCZ" {$Arguments = 'intl.cpl,,/f:"D:\temp\Config_CZ.xml"'} 
+    default {$Arguments = 'intl.cpl,,/f:"D:\temp\Config_SK.xml"'}
+}
+Write-Host "Setting Regional Settings"
+Run -Path "C:\Windows\System32\control.exe" $Arguments
 
 Write-Host ""
 # Nastavi automaticke spustanie Aplikacii na kopirovanie
