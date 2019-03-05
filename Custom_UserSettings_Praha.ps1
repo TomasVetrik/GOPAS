323a845ea9b9b047cc0845dfe9ccaccc71b65ec9
Vencess <vasekk.bernat@gmail.com>
2019-03-05 10:07:26 +0100

    Kopirovaci apka
    Udelali jsme tu kopirovaci apku pro lektory, tenhle tool jim zkopiruje file nebo folder do pozadovaneho adresare. Zatim jen v praze, apka je v Custom_OS.

--- Custom_UserSettings_Praha.ps1
+++ Custom_UserSettings_Praha.ps1
@@ -1,2 +1,4 @@
 write-host "Changing home page and setting homepage to www.gopas.cz" -ForegroundColor Green
 Set-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Internet Explorer\Main" -Name 'Start Page' -Value "http://www.gopas.cz"
+ 
+Copy-Item "D:\Temp\Kopírování na studentské PC.exe" -Destination "C:\Users\$env:username\Desktop\Kopírování na studentské PC.exe"
