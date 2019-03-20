3963924c9d075a033b5ea93c1bf84c9b4af8cc0b
vetrik3 <vetrikt@gmail.com>
2019-03-20 14:33:30 +0100

    Update Controller.ps1
    Este Mala uprava

--- Accessories/Controller.ps1
+++ Accessories/Controller.ps1
@@ -3,6 +3,10 @@ while($true)
     $Process=(Get-Process | where {$_.name -eq "rundll32"})	
     if($Process -ne $null)
 	{
+		Start-Sleep -Seconds 2
+	    [void] [System.Reflection.Assembly]::LoadWithPartialName("'Microsoft.VisualBasic")
+	    [Microsoft.VisualBasic.Interaction]::AppActivate($Process.Id)
+	    [void] [System.Reflection.Assembly]::LoadWithPartialName("'System.Windows.Forms")	
         Start-Sleep -Seconds 2
 	    [void] [System.Reflection.Assembly]::LoadWithPartialName("'Microsoft.VisualBasic")
 	    [Microsoft.VisualBasic.Interaction]::AppActivate($Process.Id)
