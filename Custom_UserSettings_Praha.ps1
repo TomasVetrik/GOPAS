bc46d2643f96809d5f4f4deed37061f6eed341f9
Vencess <vasekk.bernat@gmail.com>
2019-07-24 16:50:18 +0200

    Update Custom_UserSettings_Praha.ps1

--- Custom_UserSettings_Praha.ps1
+++ Custom_UserSettings_Praha.ps1
@@ -10,24 +10,22 @@ Copy-Item "D:\Temp\KopirovaninaStudentskePC.exe" -Destination "C:\Users\$env:use
 
 $VideoController = Get-WmiObject -class "Win32_VideoController"
 write-host "Setting $Temp\GOPAS_Background.bmp as desktop background..." -foregroundcolor green 
-if($VideoController.CurrentHorizontalResolution -eq "2560")
+switch($VideoController.CurrentHorizontalResolution)
 {
-	Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name Wallpaper -Value "$Temp\GOPAS_Background_2560x1080.png"
-	Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name WallpaperStyle -Value "2"
+	"2560"
+	{
+		$GopasBackgroundPath = "$Temp\GOPAS_Background_2560x1080.png"
+		$ScreenSaverRegistryPath = "C:\Program Files\Screensaver\screensaver2560x1080.reg"	
+	}
+	"1920"
+	{
+		$GopasBackgroundPath = "$Temp\GOPAS_Background_1920x1080.bmp"
+		$ScreenSaverRegistryPath = "C:\Program Files\Screensaver\screensaver1920x1080.reg"	
+	}
 }
-else
-{
-	Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name Wallpaper -Value "$Temp\GOPAS_Background_1920x1080.bmp"
-	Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name WallpaperStyle -Value "2"
-}
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name Wallpaper -Value $GopasBackgroundPath
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name WallpaperStyle -Value "2"
 
 #Nastaveni screensaveru - musi byt zde kvuli Multiprofile instalaci (pod uctem StudentEN jinak Screensaver nefunguje)
 write-host "Setting screensaver..." -foregroundcolor green
-if($VideoController.CurrentHorizontalResolution -eq "2560")
-{
-	regedit /s "C:\Program Files\Screensaver\screensaver2560x1080.reg"			
-}
-else
-{
-	regedit /s "C:\Program Files\Screensaver\screensaver1920x1080.reg"
-}
+regedit /s $ScreenSaverRegistryPath			
