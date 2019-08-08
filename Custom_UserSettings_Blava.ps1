7dd154a599765376c65f77d36d774354ca1d3d28
vetrik3 <vetrikt@gmail.com>
2019-08-08 10:42:20 +0200

    Pridanie pozadia pre UHD monitory

--- Custom_UserSettings_Blava.ps1
+++ Custom_UserSettings_Blava.ps1
@@ -1,3 +1,6 @@
+#Definice promennych
+$Temp="D:\Temp"
+
 . D:\Functions.ps1
 
 switch -wildcard ($env:USERNAME) 
@@ -12,3 +15,20 @@ Run -Path "C:\Windows\System32\control.exe" $Arguments
 
 write-host "Changing home page and setting homepage to www.gopas.sk" -ForegroundColor Green
 Set-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Internet Explorer\Main" -Name 'Start Page' -Value "http://www.gopas.sk"
+
+
+$VideoController = Get-WmiObject -class "Win32_VideoController"
+write-host "Setting $Temp\GOPAS_Background.bmp as desktop background..." -foregroundcolor green 
+switch($VideoController.CurrentHorizontalResolution)
+{
+	"2560"
+	{
+		$GopasBackgroundPath = "$Temp\GOPAS_Background_2560x1080.png"
+	}
+	"1920"
+	{
+		$GopasBackgroundPath = "$Temp\GOPAS_Background_1920x1080.bmp"	
+	}
+}
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name Wallpaper -Value $GopasBackgroundPath
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name WallpaperStyle -Value "2"
