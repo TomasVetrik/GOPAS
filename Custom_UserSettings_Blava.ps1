4586e0422944e410b1957dfdc99db32515c5a8c3
vetrik3 <vetrikt@gmail.com>
2020-07-17 16:08:38 +0200

    Update Custom_UserSettings_Blava.ps1

--- Custom_UserSettings_Blava.ps1
+++ Custom_UserSettings_Blava.ps1
@@ -16,7 +16,6 @@ Run -Path "C:\Windows\System32\control.exe" $Arguments
 write-host "Changing home page and setting homepage to www.gopas.sk" -ForegroundColor Green
 Set-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Internet Explorer\Main" -Name 'Start Page' -Value "http://www.gopas.sk"
 
-
 $VideoController = Get-WmiObject -class "Win32_VideoController"
 write-host "Setting $Temp\GOPAS_Background.bmp as desktop background..." -foregroundcolor green 
 $GopasBackgroundPath = "$Temp\GOPAS_Background_1920x1080.bmp"
@@ -24,11 +23,13 @@ switch($VideoController.CurrentHorizontalResolution)
 {
 	"2560"
 	{
-		$GopasBackgroundPath = "$Temp\GOPAS_Background_2560x1080.png"
+		$GopasBackgroundPath = "$Temp\GOPAS_Background_2560x1080.jpg"
 	}	
 }
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name Wallpaper -Value $GopasBackgroundPath
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name WallpaperStyle -Value "2"
+
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters, 1, true
 
 SaveComputersInfos
 
